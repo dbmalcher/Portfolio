d4785c31b6d89a19c67f2a96f9483ccfbf704659
Daniel Malcher <danielmalcherdm@gmail.com>
2026-04-28 23:23:50 -0400

    Fix error

--- frontend/src/components/organisms/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Window } from './Window';
-export { default as Taskbar } from './Taskbar';
-export { default as StartMenu } from './StartMenu';
-export { default as Desktop } from './Desktop';
