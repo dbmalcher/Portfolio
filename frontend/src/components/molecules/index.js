a2fc16ddf791729613d469746320c5e84ea8aedf
Daniel Malcher <danielmalcherdm@gmail.com>
2026-05-02 22:26:03 -0400

    Updates

--- frontend/src/components/molecules/index.js
+++
@@ -1,5 +0,0 @@
-export { default as WindowTitleBar } from './WindowTitleBar';
-export { default as WindowNavigation } from './WindowNavigation';
-export { default as MenuItem } from './MenuItem';
-export { default as TaskbarWindow } from './TaskbarWindow';
-export { default as DesktopIcon } from './DesktopIcon';
